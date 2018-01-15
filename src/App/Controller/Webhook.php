38cd7726419b501fd05db733b49ad8c301fef7da
Albert Garcia <albert.garcia@uvinum.com>
2018-01-15 02:23:57 +0100

    Implement `/insights` Telegram command

--- src/App/Controller/Webhook.php
+++ src/App/Controller/Webhook.php
@@ -2,6 +2,13 @@
 
 namespace App\Controller;
 
+use Obokaman\StockForecast\Application\Service\GetSignalsFromForecast;
+use Obokaman\StockForecast\Application\Service\GetSignalsFromForecastRequest;
+use Obokaman\StockForecast\Application\Service\PredictStockValue;
+use Obokaman\StockForecast\Application\Service\PredictStockValueRequest;
+use Obokaman\StockForecast\Domain\Model\Financial\StockDateInterval;
+use Obokaman\StockForecast\Domain\Service\Signal\CalculateScore;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use TelegramBot\Api\BotApi;
@@ -9,8 +16,17 @@ use TelegramBot\Api\Client as TelegramClient;
 use TelegramBot\Api\Exception as TelegramException;
 use TelegramBot\Api\Types\Message as TelegramMessage;
 
-class Webhook
+class Webhook extends Controller
 {
+    private $stock_predict_service;
+    private $get_signals_service;
+
+    public function __construct(PredictStockValue $a_stock_predict_service, GetSignalsFromForecast $a_get_signals_service)
+    {
+        $this->stock_predict_service = $a_stock_predict_service;
+        $this->get_signals_service   = $a_get_signals_service;
+    }
+
     public function telegram(string $token): JsonResponse
     {
         if ($token !== $_SERVER['TELEGRAM_BOT_TOKEN'])
@@ -23,12 +39,40 @@ class Webhook
 
         try
         {
+            $webhook = $this;
+
             $bot->command(
-                'hello',
-                function (TelegramMessage $message) use ($bot) {
+                'start',
+                function (TelegramMessage $a_message) use ($bot) {
                     $welcome_message =
                         'Hey there. Welcome to Crypto Insights bot. You can use bot commands to get some insights, predictions and recommendations for your favorite cryptos.';
-                    $bot->sendMessage($message->getChat()->getId(), $welcome_message);
+                    $bot->sendMessage($a_message->getChat()->getId(), $welcome_message);
+                }
+            );
+
+            $bot->command(
+                'insights',
+                function (TelegramMessage $message) use ($bot, $webhook) {
+                    $params = explode(' ', $message->getText());
+
+                    $currency = strtoupper($params[1] ?? 'EUR');
+                    $crypto   = strtoupper($params[2] ?? 'BTC');
+
+                    $bot->sendMessage(
+                        $message->getChat()->getId(),
+                        sprintf('I\'ll give you some insights for *%s-%s*:', $currency, $crypto),
+                        'Markdown'
+                    );
+
+                    $signals_message = $webhook->outputSignalsBasedOn('hour', StockDateInterval::MINUTES, $currency, $crypto);
+                    $signals_message .= $webhook->outputSignalsBasedOn('day', StockDateInterval::HOURS, $currency, $crypto);
+                    $signals_message .= $webhook->outputSignalsBasedOn('month', StockDateInterval::DAYS, $currency, $crypto);
+
+                    $bot->sendMessage(
+                        $message->getChat()->getId(),
+                        $signals_message,
+                        'Markdown'
+                    );
                 }
             );
 
@@ -45,5 +89,26 @@ class Webhook
         }
 
         return new JsonResponse([]);
+    }
+
+    private function outputSignalsBasedOn(string $interval, string $interval_unit, string $currency, string $stock): string
+    {
+        $prediction_request  = new PredictStockValueRequest($currency, $stock, $interval_unit);
+        $prediction_response = $this->stock_predict_service->predict($prediction_request);
+        $signals_request     = new GetSignalsFromForecastRequest(
+            $prediction_response->shortTermStats(),
+            $prediction_response->mediumTermStats(),
+            $prediction_response->longTermStats()
+        );
+        $signals_response    = $this->get_signals_service->getSignals($signals_request);
+
+        $message = 'Based on *last ' . $interval . '* (Score: ' . CalculateScore::calculate(...$signals_response) . '):' . PHP_EOL;
+
+        foreach ($signals_response as $signal)
+        {
+            $message .= '_ - ' . $signal . '_' . PHP_EOL;
+        }
+
+        return $message . PHP_EOL;
     }
 }
